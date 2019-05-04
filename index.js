7d845dcbeecc60ad824b3bd75b977e4a52c1077d
Rebecca <rrossman25@gmail.com>
2019-05-04 16:24:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(name){
+  
+}
+
+function destructivelyPrependKitten(name){
+  
+}
+
+function destructivelyRemoveLastKitten(){
+  
+}
+
+function destructivelyRemoveFirseKitten(){
+  
+}
+
+function appendKitten(name){
+  
+}
+
+function prependKitten(name){
+  
+}
+
+function removeLastKitten(){
+  
+}
+
+function removeFirstKitten(){
+  
+}
