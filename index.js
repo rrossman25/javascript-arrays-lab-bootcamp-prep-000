b4d96c73889278b45072ac6b8620e0fb2a16578d
Rebecca <rrossman25@gmail.com>
2019-05-04 16:26:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,19 +3,23 @@ var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 // Add your functions and code here
 
 function destructivelyAppendKitten(name){
-  
+  kittens.push(name);
+  return kittens;
 }
 
 function destructivelyPrependKitten(name){
-  
+  kittens.unshift(name);
+  return kittens;
 }
 
 function destructivelyRemoveLastKitten(){
-  
+  let kitkat = kittens.pop();
+  return kittens;
 }
 
 function destructivelyRemoveFirseKitten(){
-  
+  let kitkat = kittens.shift();
+  return kittens;
 }
 
 function appendKitten(name){
